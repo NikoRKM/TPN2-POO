219d05295fa407bef0d77e8c02e63837d397cbf9
Martinchx <martinruz1426@gmail.com>
2022-08-21 02:20:50 -0300

    Point10 & Point11 & Point12

--- src/main/java/ar/edu/unju/edm/tpn2/points/Point10.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point10.java
@@ -1,5 +1,21 @@
 package ar.edu.unju.edm.tpn2.points;
 
+import java.util.Scanner;
+
 public class Point10 {
 
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 5; ++i){
+
+            System.out.println("Enter an age: ");
+            int age = scanner.nextInt();
+
+            if(age >= 1 && age <= 12) System.out.println("Is a kid\n");
+            else if(age >= 13 && age <= 20) System.out.println("Is a youth\n");
+            else if(age >= 21 && age <= 100) System.out.println("Is an adult\n");
+            else System.out.println("Wrong input\n");
+        }
+    }
 }
--- src/main/java/ar/edu/unju/edm/tpn2/points/Point12.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point12.java
@@ -1,5 +1,38 @@
 package ar.edu.unju.edm.tpn2.points;
 
+import java.util.Scanner;
+
 public class Point12 {
 
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+        String[] array = new String[5];
+
+        for(int i = 0; i < array.length; ++i){
+
+            System.out.println("\nEnter a name: ");
+            array[i] = scanner.nextLine();
+
+        }
+
+        System.out.println("\nValues in the array: ");
+
+        for(int i = 0; i < array.length; ++i){
+            System.out.println("[" + i + "]: " + array[i]);
+        }
+
+        System.out.println("\nEnter the index from where you want to delete the value: ");
+        int index = scanner.nextInt();
+        
+        for(int i = index; i < array.length - 1; ++i){
+            array[i] = array[i+1];
+        }
+
+        System.out.println("\nValues in the array after deleting the value: ");
+
+        for(int i = 0; i < array.length - 1; ++i){
+            System.out.println("[" + i + "]: " + array[i]);
+        }
+    }
 }
