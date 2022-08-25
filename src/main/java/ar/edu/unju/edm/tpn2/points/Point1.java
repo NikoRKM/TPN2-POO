2745af1078a34bfa1c0c1042c6612074a992eaef
Marcos Quinteros <Usuario@DESKTOP-QEN7T49>
2022-08-25 17:01:57 -0300

    Point 1 y 6

--- src/main/java/ar/edu/unju/edm/tpn2/points/Point1.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point1.java
@@ -2,6 +2,7 @@ package ar.edu.unju.edm.tpn2.points;
 
 public class Point1 {
 	public static void main(String[] args){
+		
 		String punto_1 = "¡Hola mundo! Programación Orientada a Objetos";
 		System.out.println(punto_1);
 		
--- src/main/java/ar/edu/unju/edm/tpn2/points/Point6.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point6.java
@@ -1,13 +1,9 @@
 package ar.edu.unju.edm.tpn2.points;
 
-import java.util.Scanner;
-
 public class Point6 {
 		
 		public static void main(String args[]) {
 			
-	        Scanner variable = new Scanner(System.in);
-	        String numero;
 	        int a=5, b=10, c=2, d=6;
 		
 	        System.out.println("a = " + a + " b = " + b + " c = " + c + " d = " + d);
