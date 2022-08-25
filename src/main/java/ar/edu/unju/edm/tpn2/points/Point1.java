9f0b3e922c2f3def31afec059d26ca62ccba0436
Usuario <Usuario@DESKTOP-QEN7T49>
2022-08-25 16:43:53 -0300

    Point 1,6,9

--- src/main/java/ar/edu/unju/edm/tpn2/points/Point1.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point1.java
@@ -1,5 +1,10 @@
 package ar.edu.unju.edm.tpn2.points;
 
 public class Point1 {
-
+	public static void main(String[] args){
+		String punto_1 = "Programación Orientada a Objetos";
+		String saludo = "¡Hola mundo!";
+		System.out.println(saludo + " " + punto_1);
+		
+		}
 }
--- src/main/java/ar/edu/unju/edm/tpn2/points/Point9.java
+++ src/main/java/ar/edu/unju/edm/tpn2/points/Point9.java
@@ -1,5 +1,21 @@
 package ar.edu.unju.edm.tpn2.points;
 
-public class Point9 {
+import java.util.Scanner;
 
+public class Point9 {
+	
+	public static void main(String args[]) {
+	    
+		for(int contador = 0;contador<5;contador++) {
+	        Scanner variable = new Scanner(System.in);
+	        int numero ;
+	        System.out.println("Ingrese un numero:");
+	        numero = variable.nextInt();
+	
+	        if (numero % 2 == 0)
+	            System.out.println("El numero es par");
+	        else
+	            System.out.println("El numero es impar");
+	    	}
+	   }
 }
